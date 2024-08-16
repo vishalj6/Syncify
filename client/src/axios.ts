488f302c437464c37ef94a457e50944655a0ae15
vishalj6 <vishaljadeja6@gmail.com>
2024-08-16 18:27:54 +0530

    updated axios timeout

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an instance of axios with custom configuration
 const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_BACKENDURI}`,
-    timeout: 5000,
+    // timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
     },
